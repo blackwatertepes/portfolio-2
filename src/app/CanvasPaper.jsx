2b1895208c693c0af60ba38da484346119af1e90
Tyler Kuhn <tyler@wargoats.com>
2024-06-01 21:32:17 -0700

    use the window as the x,y store

--- src/app/CanvasPaper.jsx
+++ src/app/CanvasPaper.jsx
@@ -6,20 +6,14 @@ export default function CanvasPaper() {
   return (                                                                                   
     <>                                                                                       
       <Script src="https://cdnjs.cloudflare.com/ajax/libs/paper.js/0.12.17/paper-full.min.js" onLoad={() => {
-        let width = document.getElementById("canvasPaper").getAttribute("width", window.innerWidth);
-        let height = document.getElementById("canvasPaper").getAttribute("height", window.innerHeight);
-
         window.addEventListener("load", (_) => {
-          width = window.innerWidth;
-          height = window.innerHeight;
-          document.getElementById("canvasPaper").setAttribute("width", width);
-          document.getElementById("canvasPaper").setAttribute("height", height);
+          document.getElementById("canvasPaper").setAttribute("width", window.innerWidth);
+          document.getElementById("canvasPaper").setAttribute("height", window.innerHeight);
         });
+
         window.addEventListener("resize", (_) => {
-          width = window.innerWidth;
-          height = window.innerHeight;
-          document.getElementById("canvasPaper").setAttribute("width", width);
-          document.getElementById("canvasPaper").setAttribute("height", height);
+          document.getElementById("canvasPaper").setAttribute("width", window.innerWidth);
+          document.getElementById("canvasPaper").setAttribute("height", window.innerHeight);
         });
 
         let canvas = document.getElementById("canvasPaper");                                 
@@ -30,7 +24,7 @@ export default function CanvasPaper() {
           const x = Math.random();
           const y = Math.random();
           const speed = Math.random() * 0.0002 + 0.0002;
-          let start = new paper.Point(x * width, y * height);
+          let start = new paper.Point(x * window.innerWidth, y * window.innerHeight);
           let size = new paper.Size(1, 1);
           let shape = new paper.Shape.Rectangle(start, size);                                
           shape.style = { 
@@ -42,8 +36,8 @@ export default function CanvasPaper() {
         setInterval(() => {                                                                  
           sprites.forEach(sprite => {                                                        
             sprite.x += sprite.speed;                                         
-            sprite.shape.position.x = sprite.x * width;
-            sprite.shape.position.y = sprite.y * height;
+            sprite.shape.position.x = sprite.x * window.innerWidth;
+            sprite.shape.position.y = sprite.y * window.innerHeight;
             if (sprite.x > 1) {
               sprite.x = 0;
             }
